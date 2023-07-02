e9f65f3c63b12e386db84eab36db5a9909f72c8a
unfode <forrest.futao.wei@gmail.com>
2023-07-02 15:10:45 -0400

    Improve format

--- bin/main.ml
+++ bin/main.ml
@@ -1,21 +1,23 @@
 open Lib.Interpreter
 open Lib.Utility
 
-let () = if (Array.length Sys.argv) != 2 then
-  print_endline "Usage: ocaml_lox <file path>"
-else (
-  match read_file Sys.argv.(1) with
-  | Error e -> print_endline (Printexc.to_string e)
-  | Ok source -> (
-    match scan source with
-    | Error -> print_endline "an error was found during scanning"
-    | Correct tokens -> (
-      match parse tokens with
-      | Error -> print_endline "an error was found during parsing"
-      | Correct statements -> (
-        match execute_statements statements with
-        | Error -> print_endline "an error was found during interpreting"
-        | Correct -> ()
+let () = (
+  if (Array.length Sys.argv) != 2 then
+    print_endline "Usage: ocaml_lox <file path>"
+  else (
+    match read_file Sys.argv.(1) with
+    | Error e -> print_endline (Printexc.to_string e)
+    | Ok source -> (
+      match scan source with
+      | Error -> print_endline "an error was found during scanning"
+      | Correct tokens -> (
+        match parse tokens with
+        | Error -> print_endline "an error was found during parsing"
+        | Correct statements -> (
+          match execute_statements statements with
+          | Error -> print_endline "an error was found during interpreting"
+          | Correct -> ()
+        )
       )
     )
   )
